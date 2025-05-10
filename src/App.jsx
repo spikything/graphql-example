377e1545501defd06cbf2c35a4f02c0266529b47
Liam O <spikything@gmail.com>
2025-05-10 11:10:45 +0100

    Extract `Countries` component

--- src/App.jsx
+++ src/App.jsx
@@ -1,92 +1,10 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  gql,
-  useLazyQuery,
-  useQuery,
-} from "@apollo/client";
-import { useState } from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import Countries from "./components/Countries";
 
 const client = new ApolloClient({
   uri: "https://countries.trevorblades.com/",
   cache: new InMemoryCache(),
 });
-
-const GET_CONTINENTS = gql`
-  query {
-    continents {
-      code
-      name
-    }
-  }
-`;
-
-const GET_COUNTRIES_BY_CONTINENT = gql`
-  query CountriesByContinent($code: ID!) {
-    continent(code: $code) {
-      name
-      countries {
-        code
-        name
-      }
-    }
-  }
-`;
-
-function Countries() {
-  const {
-    loading: continentsLoading,
-    error: continentsError,
-    data: continentsData,
-  } = useQuery(GET_CONTINENTS);
-  const [fetchCountries, { loading, error, data }] = useLazyQuery(
-    GET_COUNTRIES_BY_CONTINENT
-  );
-
-  const [selectedCode, setSelectedCode] = useState("");
-
-  const handleSelect = (e) => {
-    const code = e.target.value;
-    setSelectedCode(code);
-    if (code) {
-      fetchCountries({ variables: { code } });
-    }
-  };
-
-  if (continentsLoading) return <p>Loading continents...</p>;
-  if (continentsError) return <p>Error loading continents 😢</p>;
-
-  return (
-    <div>
-      <label>
-        Select continent<br />
-        <select value={selectedCode} onChange={handleSelect}>
-          <option value="">-- Select --</option>
-          {continentsData.continents.map((continent) => (
-            <option key={continent.code} value={continent.code}>
-              {continent.name}
-            </option>
-          ))}
-        </select>
-      </label>
-
-      {loading && <p>Loading countries...</p>}
-      {error && <p>Error loading countries 😢</p>}
-
-      {data && (
-        <>
-          <h2>{data.continent.name}</h2>
-          <ul>
-            {data.continent.countries.map((country) => (
-              <li key={country.code}>{country.name}</li>
-            ))}
-          </ul>
-        </>
-      )}
-    </div>
-  );
-}
 
 function App() {
   return (
